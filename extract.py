f46d92ed140d50ec7fc53a34a01cde68a518fa60
icewindfour <icewindfour@gmail.com>
2022-12-08 12:09:55 +0000

    formatting

--- extract.py
+++ extract.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+# from pathlib import Path
 import pandas as pd
 import numpy as np
 
@@ -16,15 +16,14 @@ to_drop = [
     'card_number'
     ]
 
-file_path = "reports_from_branches/chesterfield_25-08-2021_09-00-00.csv"
+# file_path = "reports_from_branches/chesterfield_25-08-2021_09-00-00.csv"
 
 def turn_file_into_dataframe(file_path, col_names):
-    df = pd.read_csv(Path(file_path), names=col_names)
-    df.head()
+    df = pd.read_csv(file_path, names=col_names)
     df = df.drop(to_drop, axis=1)
     return df
 
-df = turn_file_into_dataframe(file_path, col_names)
+df = turn_file_into_dataframe("reports_from_branches/chesterfield_25-08-2021_09-00-00.csv", col_names)
 
 print(df)
 
