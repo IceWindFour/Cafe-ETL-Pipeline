99dfd1194ed670f89b8da5e989b01b8bf225b49d
icewindfour <icewindfour@gmail.com>
2022-12-08 11:27:03 +0000

    extraction

---
+++ extract.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+col_names = [
+    "timestamp",
+    "store_name",
+    "customer_name",
+    "products",
+    "total_price",
+    "payment_method",
+    "card_number"
+]
+to_drop = [
+    'customer_name',
+    'card_number'
+    ]
+
+file_path = "reports_from_branches/chesterfield_25-08-2021_09-00-00.csv"
+
+def turn_file_into_dataframe(file_path, col_names):
+    df = pd.read_csv(Path(file_path), names=col_names)
+    df.head()
+    df = df.drop(to_drop, axis=1)
+    return df
+
+df = turn_file_into_dataframe(file_path, col_names)
+
+print(df)
+
