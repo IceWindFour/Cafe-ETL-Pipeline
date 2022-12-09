b0d1e1df48e79cde3b01e60b9736d9a51f57ac9b
icewindfour <icewindfour@gmail.com>
2022-12-09 14:09:12 +0000

    blue format

--- extract.py
+++ extract.py
@@ -3,27 +3,27 @@ import pandas as pd
 import numpy as np
 
 col_names = [
-    "timestamp",
-    "store_name",
-    "customer_name",
-    "products",
-    "total_price",
-    "payment_method",
-    "card_number"
-]
-to_drop = [
+    'timestamp',
+    'store_name',
     'customer_name',
-    'card_number'
-    ]
+    'products',
+    'total_price',
+    'payment_method',
+    'card_number',
+]
+to_drop = ['customer_name', 'card_number']
 
 # file_path = "reports_from_branches/chesterfield_25-08-2021_09-00-00.csv"
+
 
 def turn_file_into_dataframe(file_path, col_names):
     df = pd.read_csv(file_path, names=col_names)
     df = df.drop(to_drop, axis=1)
     return df
 
-df = turn_file_into_dataframe("reports_from_branches/chesterfield_25-08-2021_09-00-00.csv", col_names)
+
+df = turn_file_into_dataframe(
+    'reports_from_branches/chesterfield_25-08-2021_09-00-00.csv', col_names
+)
 
 print(df)
-
