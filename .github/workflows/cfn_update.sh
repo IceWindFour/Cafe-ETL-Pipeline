016bd22e9529a20861782798f4f1452f886b2022
Daniel Lui <DanielLui@Daniels-iMac.local>
2023-01-13 22:30:30 +0000

    evening

--- .github/workflows/cfn_update.sh
+++ .github/workflows/cfn_update.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
 # Get the file name and stack name
-file=$1
-stack_name=$2
+cloudformation_file=$1
 
-# Get the hashes of the files
-hash1=$(md5sum $file | awk '{print $1}')
-hash2=$(aws cloudformation describe-stacks --stack-name $stack_name --query "Stacks[0].TemplateBody" | md5sum | awk '{print $1}')
+# aws download the hash.txt from s3 bucket
+aws s3 cp s3://oldmengrinding-cicd/hash.txt hash.txt
 
-# Compare the hashes
-if [ "$hash1" != "$hash2" ]; then
-    echo "The files are different, updating the stack..."
-    aws cloudformation update-stack --stack-name $stack_name --template-body file://$file
-else
-    echo "The files are identical, no update needed."
-fi
+
+# hash_file="$(<hash.txt)"
+# value="$(<$hash_file)"
+
+
+# # Get the hashes of the files
+# hash1=$(md5sum $file | awk '{print $1}')
+# hash2=$(echo $value | awk '{print $1;}')
+
+
+# # Compare the hashes
+# if [ "$hash1" != "$hash2" ]; then
+#     echo "The files are different, updating the stack..."
+#     aws cloudformation update-stack --stack-name $stack_name --template-body file://$file
+
+    # upload the updated hash into a txt in oldmengrinding bucket
+    # update the stack
+# else
+#     echo "The files are identical, no update needed."
+# fi
