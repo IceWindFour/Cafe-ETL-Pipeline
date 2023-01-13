d2a4344ba1bbd0a0dfd5767f7f44f1dd22f75506
icewindfour <icewindfour@gmail.com>
2023-01-13 14:30:42 +0000

    stuff

--- data_db/create_tables.py
+++ data_db/create_tables.py
@@ -20,7 +20,7 @@ cur.execute("""CREATE TABLE IF NOT EXISTS products
 """)
 
 cur.execute("""CREATE TABLE IF NOT EXISTS transactions
-    (transaction_id NUMERIC PRIMARY KEY,
+    (transaction_id numeric PRIMARY KEY,
 branch TEXT NOT NULL,
 total_price FLOAT,
 payment_type TEXT,
@@ -29,9 +29,17 @@ date_time TIMESTAMP)
 
 cur.execute("""CREATE TABLE IF NOT EXISTS baskets
     (basket_item_id SERIAL PRIMARY KEY,
-    transaction_id NUMERIC,
+    transaction_id numeric,
     product_name TEXT)
 """)
+
+# cur.execute("""ALTER TABLE baskets
+#     ADD CONSTRAINT fk_baskets_transaction_id FOREIGN KEY(transaction_id) REFERENCES transactions(transaction_id);
+# """)
+
+# cur.execute("""ALTER TABLE baskets
+#     ADD CONSTRAINT fk_baskets_product_id FOREIGN KEY(product_id) REFERENCES products(product_id);
+# """)
 
 
 print("Table created successfully")
